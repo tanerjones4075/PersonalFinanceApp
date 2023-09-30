d54b09cecb03d4593450c03ba105561d7f080175
Tanner Jones <78619684+tanerjones4075@users.noreply.github.com>
2023-09-29 21:32:37 -0800

    Intial Commit

---
+++ PersonalFinanceApp/transactionRestAPI/cmd/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/tannerjones4075/TransactionsRestApi/transactionRestAPI/cmd/pkg/handlers"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the Transactions REST API!")
+	fmt.Println("Transactions REST API")
+}
+
+func handleRequests() {
+	// create a new instance of a mux router
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8080", myRouter))
+}
+
+func handleRequests() {
+	// create a new instance of a mux router
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/transactions", handlers.GetAllTransactions).Methods(http.MethodGet)
+	log.Fatal(http.ListenAndServe(":8080", myRouter))
+}
+
+func main() {
+	handleRequests()
+
+}
