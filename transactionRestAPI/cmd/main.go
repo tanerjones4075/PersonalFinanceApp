3f7d6de51440843df2583f21745fcaffd31ef71f
Tanner Jones <alltechguyblog@gmail.com>
2023-09-29 22:08:27 -0800

    initial commit

--- transactionRestAPI/cmd/main.go
+++ transactionRestAPI/cmd/main.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/tannerjones4075/transactionRestAPI/pkg/handlers"
+	"github.com/tannerjones4075/PersonalFinanceApp/transactionRestAPI/pkg/handlers"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
