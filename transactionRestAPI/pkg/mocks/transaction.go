3483fba4be9342e6e81d2842ba38579cd52e0305
Tanner Jones <alltechguyblog@gmail.com>
2023-09-29 23:05:57 -0800

    Updated imports

--- transactionRestAPI/pkg/mocks/transaction.go
+++ transactionRestAPI/pkg/mocks/transaction.go
@@ -1,7 +1,7 @@
 package mocks
 
 import (
-	"github.com/tannerjones4075/PersonalFinanceApp/transactionRestApi/pkg/models"
+	"github.com/tannerjones4075/PersonalFinanceApptransactionRestAPI/pkg/models"
 
 ) 
 var Articles = []models.Transaction{
